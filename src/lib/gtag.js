5a7fb98569d6f293597a167d5192813b79764f9f
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-10-13 14:52:48 +0300

    gtag

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,24 +1,17 @@
 export const GA_TRACKING_ID = 'UA-180341827-1' // This is your GA Tracking ID
 
-const isBrowser = typeof window !== 'undefined'
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  if (isBrowser && isProduction) {
-    window.gtag('config', GA_TRACKING_ID, {
-      page_path: url,
-    })
-  }
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  if (isBrowser && isProduction) {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    })
-  }
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
 }
