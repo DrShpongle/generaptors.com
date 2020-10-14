ff34df7be48b2d439140541f0d0e95d4d6c97e29
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-10-14 13:15:29 +0300

    lang

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
   render() {
     const { isProduction } = this.props
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta httpEquiv="content-language" content="en-us" />
           <link
